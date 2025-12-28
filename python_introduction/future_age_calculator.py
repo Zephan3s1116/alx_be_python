0a2ea993ae82004efdf74d82dd668bec16227edd
zephan <zephan3s1.16@gamil.com>
2025-12-28 18:54:13 +0300

    Complete all introduction tasks

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+user_age = int(input("How old are you? "))
+# Calculating age in 2050 (2050 - 2023 = 27 years to add)
+age_in_2050 = user_age + 27
+
+print(f"In 2050, you will be {age_in_2050} years old.")
